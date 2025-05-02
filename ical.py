280fd194eae16088e7d13837b80bb7228f56e313
Sean Rodman <38499313+bassman7689@users.noreply.github.com>
2025-05-02 06:16:16 -0500

    initial commit.

---
+++ ical.py
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2025 Sean Rodman
+#
+# SPDX-License-Identifier: MIT
+"""
+`ical`
+================================================================================
+
+ICAL parser for CircuitPython
+
+
+* Author(s): Sean Rodman
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s).
+  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
+  based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+"""
+
+# imports
+import typing
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/bassman7689/CircuitPython_ical.git"
+
+
+
+class ICal():
+    def __init__(file):
